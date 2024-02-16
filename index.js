af320c9f3d427e0c0ec0d610873f054389da653d
pablofm <pablofm@us.es>
2024-02-16 11:56:27 +0100

    initial code

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 let cool = require("cool-ascii-faces");
+let express = require("express");
 
-console.log(cool());
+let app = express();
+const PORT = (process.env.PORT || 10000);
+
+app.get("/", (req,res)=>{
+    res.send("<html><body><h1>Hola Mundo!</h1></body></html>");
+});
+
+app.get("/cool", (req,res)=>{
+    res.send(`<html><body><h1>${cool()}</h1></body></html>`);
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server listening on port ${PORT}.`);
+});
+
+console.log(`Server initializing...`);
