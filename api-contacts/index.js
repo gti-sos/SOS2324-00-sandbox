9318f003289a87502e636b09e1a3d98b714d548f
pablofm <pablofm@us.es>
2024-03-01 11:35:39 +0100

    base api with nedb

--- api-contacts/index.js
+++ api-contacts/index.js
@@ -1,6 +1,6 @@
 const API_BASE="/api/v1";
 
-var contacts = [
+var initialContacts = [
     {
         name: "pepe",
         phone: 12345
@@ -11,16 +11,50 @@ var contacts = [
     }
 ];
 
-module.exports = (app) => {
+module.exports = (app,db) => {
+
+    app.get(API_BASE+"/loadInitialContacts",(req,res)=>{
+        db.insert(initialContacts);
+        res.sendStatus(200,"Ok");
+    });
+
 
     app.get(API_BASE+"/contacts",(req,res)=>{
-        res.send(JSON.stringify(contacts));
+
+        db.find({},(err,contacts)=>{
+
+            if(err){
+                res.sendStatus(500,"Internal Error");
+            }else{
+                res.send(JSON.stringify(contacts.map((c)=>{
+                    delete c._id;
+                    return c;
+                })));  
+            }
+        });
     });
     
     app.post(API_BASE+"/contacts",(req,res)=>{
         let contact = req.body;
-        contacts.push(contact);
+        db.insert(contact);
         res.sendStatus(201,"Created");
+    });
+
+    app.delete(API_BASE+"/contacts/:name",(req,res)=>{
+        let name = req.params.name;
+
+        db.remove({ "name": name},{},(err,numRemoved)=>{
+            if(err){
+                res.sendStatus(500,"Internal Error");
+            }else{
+               if(numRemoved >= 1){
+                    res.sendStatus(200,"Ok");
+               } else {
+                    res.sendStatus(404,"Not found");
+               }
+            }
+        });
+        
     });
 
 };
--- index.js
+++ index.js
@@ -2,15 +2,17 @@ let cool = require("cool-ascii-faces");
 let express = require("express");
 let bodyParser = require("body-parser");
 let contactAPI = require("./api-contacts");
+let dataStore = require("nedb");
+
+let dbContacts = new dataStore();
 
 let app = express();
-
 
 const PORT = (process.env.PORT || 10000);
 
 app.use(bodyParser.json());
 
-contactAPI(app);
+contactAPI(app,dbContacts);
 
 app.use("/",express.static("./public"));
 
